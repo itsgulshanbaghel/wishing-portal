c7d53dcecee0be3edd3089d153090e64a560b7bc
Gulshan Baghel <itsgulshanbaghel@gmail.com>
2026-05-26 19:54:36 +0530

    SEO Fixed

---
+++ public/__/firebase/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyBAwHkE42ENs7lYS5X5xlxIYeZyNPTrIaM",
+  "appId": "1:538627248845:web:f72f598430ce87bf178f84",
+  "authDomain": "thegreeterindia.firebaseapp.com",
+  "databaseURL": "",
+  "measurementId": "G-C57MDJX3TJ",
+  "messagingSenderId": "538627248845",
+  "projectId": "thegreeterindia",
+  "storageBucket": "thegreeterindia.firebasestorage.app"
+});
